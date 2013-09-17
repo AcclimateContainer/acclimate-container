b73d1588a4f0558087265d6fe75ac787293111ba
Jeremy Lindblom <jeremeamia@gmail.com>
2013-09-16 17:29:28 -0700

    Minor adjustments to composer.json and the test boostrap

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,4 +1,8 @@
 <?php
 
+if (PHP_VERSION_ID < 50400) {
+    die('Please use PHP 5.4 or later for testing. Currently, you are using PHP ' . PHP_VERSION . '.');
+}
+
 $loader = require __DIR__ . '/../vendor/autoload.php';
 $loader->add('Jeremeamia\Acclimate\Test', __DIR__);
