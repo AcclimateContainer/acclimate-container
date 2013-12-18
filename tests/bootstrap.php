f357bee5d236a7fa05fdaa115f0434e18dd26285
Jeremy Lindblom <jeremeamia@gmail.com>
2013-12-17 23:01:56 -0800

    Removed a snippet of debug code from the testing bootstrap

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,7 +1,5 @@
 <?php
 
-echo "here\n";
-
 // Ensure PHP 5.4+ for testing
 if (PHP_VERSION_ID < 50400) {
     die('Please use PHP 5.4+ for testing. Currently, you are using PHP ' . PHP_VERSION . '.');
