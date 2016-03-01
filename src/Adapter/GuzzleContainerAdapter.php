d18b11525a0e986293156f64166f9a0e3c0c4a13
Sam Burns <sam.burns.uk@gmail.com>
2016-03-01 11:11:47 +0000

    Revert "Merge pull request #26 from Sam-Burns/remove-guzzle"
    This reverts commit ebac5893672b73dc57de4654b5f7e54c6a29e8cb, reversing
    changes made to fbbe0f629d6714e6235dc1b37aface0359acbff0.

---
+++ src/Adapter/GuzzleContainerAdapter.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Acclimate\Container\Adapter;
+
+use Acclimate\Container\Exception\ContainerException as AcclimateContainerException;
+use Acclimate\Container\Exception\NotFoundException as AcclimateNotFoundException;
+use Guzzle\Service\Builder\ServiceBuilderInterface as GuzzleContainerInterface;
+use Guzzle\Service\Exception\ServiceNotFoundException as GuzzleNotFoundException;
+use Interop\Container\ContainerInterface as AcclimateContainerInterface;
+
+/**
+ * An adapter from a Guzzle ServiceBuilder to the standardized ContainerInterface
+ */
+class GuzzleContainerAdapter implements AcclimateContainerInterface
+{
+    /**
+     * @var GuzzleContainerInterface A Guzzle ServiceBuilder
+     */
+    private $container;
+
+    /**
+     * @param GuzzleContainerInterface $container A Guzzle ServiceBuilder
+     */
+    public function __construct(GuzzleContainerInterface $container)
+    {
+        $this->container = $container;
+    }
+
+    public function get($id)
+    {
+        try {
+            return $this->container->get($id);
+        } catch (GuzzleNotFoundException $prev) {
+            throw AcclimateNotFoundException::fromPrevious($id, $prev);
+        } catch (\Exception $prev) {
+            throw AcclimateContainerException::fromPrevious($id, $prev);
+        }
+    }
+
+    public function has($id)
+    {
+        return isset($this->container[$id]);
+    }
+}
